43ad3295ff7766df589308e7b2441da2614b5461
abhinav6859 <abhinavvats06665@gmail.com>
2023-12-07 22:20:54 -0800

    initial commit

---
+++ basic c programing/conditionaloperator.c
@@ -0,0 +1,15 @@
+// itis divisible by 5 and 3
+#include <stdio.h>
+int main (void)
+{ 
+    int n ;
+    printf ("enter a number ");
+    scanf ("%d \n",&n);
+if
+   ( n%5==0 && n%3==0)
+   {
+   printf (" number is divisible by 15");}
+   else {
+   printf (" not divisible by 15");
+
+}}
