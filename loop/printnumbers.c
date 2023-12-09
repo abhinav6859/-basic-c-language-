a7fe1633c9571026925f7d0fc55b760ded11da52
abhinav6859 <abhinavvats06665@gmail.com>
2023-12-08 19:13:16 -0800

    initial commit

---
+++ loop/printnumbers.c
@@ -0,0 +1,10 @@
+//  print natural  numbers from 1 to n 
+#include <stdio.h>
+int main(void)
+{ int n;
+printf ("how much number u want to print \n ");
+scanf ("%d",&n);
+for (int i=0;i<=n;i++)
+{ printf ("%d\n",i);
+}
+}
