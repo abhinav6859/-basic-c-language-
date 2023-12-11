3d73883b7ebe3c7d43831bcf98a588fb9d71fc2b
abhinav6859 <abhinavvats06665@gmail.com>
2023-12-11 03:57:00 -0800

    fix error

--- loop/primenumber.c
+++ loop/primenumber.c
@@ -5,15 +5,19 @@ int main()
     int n;
     printf("enter last digit \n ");
     scanf("%d", &n);
-     int a=0;
-    for (int i = 2; i <= n-1; i++){
-    if (n%i==0)
+    int a=0 ;
+    for (int i = 2; i <= n - 1; i++)
     {
-     int a=1;
-     break; 
+        if (n % i == 0)
+        {
+            a = 1;
+            break;
+        }
     }
-    }
-    if (a==0)  printf("number is prime");    
-   else printf(" number is composite");
-   return 0;
+    if (a==0){
+        printf("number is prime"); }
+    else
+      {printf(" number is composite");
+        }
+    return 0;
 }
