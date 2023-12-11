1ee9203abdef0de632408f983d6ccd0b757a5205
abhinav6859 <abhinavvats06665@gmail.com>
2023-12-11 01:55:16 -0800

    it's wrong ,i'll work on this later

--- loop/primenumber.c
+++ loop/primenumber.c
@@ -1,19 +1,19 @@
 // display this gp 100,50,25 .... up to n 
 #include <stdio.h>
-int main(void)
+int main()
 {
-int a=0;
     int n;
     printf("enter last digit \n ");
     scanf("%d", &n);
-    for (int i = 2; i <= n-1; i++)
+     int a=0;
+    for (int i = 2; i <= n-1; i++){
     if (n%i==0)
-    {int a=1;
-    break;}
-    if(n==1)
-    printf(" 1 number is neither prime nor composite");
-    else if  (a==1)
-    printf(" number is composite");
-    else 
-    printf("number is prime");    
+    {
+     int a=1;
+     break; 
+    }
+    }
+    if (a==0)  printf("number is prime");    
+   else printf(" number is composite");
+   return 0;
 }
